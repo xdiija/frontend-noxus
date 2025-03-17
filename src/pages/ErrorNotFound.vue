80c1ccf35fcda52b80ccbf6a4443541ceb25caac
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-03-17 20:08:40 -0300

    Configuração do novo Layout da pagina de erro

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-primary text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
-        404
+        <img
+          src="../assets/MykeErrorPage.png"
+          alt="Error Image"
+          style="width: 300px; height: auto; margin-right: 10px;"
+        />
       </div>
 
       <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
+        Ah nãooo! Você acabou se perdendo...
       </div>
 
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="primary"
         unelevated
         to="/"
-        label="Go Home"
+        label="Voltar ao inicio"
         no-caps
       />
     </div>
@@ -26,6 +30,6 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'ErrorNotFound'
+    name: 'ErrorNotFound'
 })
 </script>
